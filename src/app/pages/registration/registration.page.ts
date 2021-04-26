ed86ad1dd1982375b7c97498110c177a4a986dfa
Ulrich-Nelson <ulrichnelsontapinfo@yahoo.fr>
2021-04-26 23:56:54 +0200

    connect the application with the API

--- src/app/pages/registration/registration.page.ts
+++ src/app/pages/registration/registration.page.ts
@@ -16,6 +16,7 @@ import { ToastMessageService } from 'src/app/services/toast-message.service';
 export class RegistrationPage implements OnInit {
 
 
+//Les informations sur le RGPD
  currentPosition: any;
  height: any;
  minimumThreshold: any;
@@ -40,7 +41,7 @@ export class RegistrationPage implements OnInit {
 
 
   ngOnInit() {
-    this.close();
+    //this.close();
     this.initForm();
   }
 
@@ -84,41 +85,43 @@ registerAction(){
 }
 
 
-  /*--------------TRAITEMENT DES DONNEES RGPD----------*/
-  open(){
-    (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.bottom = "0px";
-    (<HTMLStyleElement>document.querySelector(".bg")).style.display = "block";
-  }
-
-  close(){
-    this.currentPosition = 0;
-    this.startPosition = 0;
-    (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.bottom = "-1000px";
-    (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.transform = "translate3d(0,0,0)";
-    (<HTMLStyleElement>document.querySelector(".bg")).style.display = "none";
-
-  }
-
-  touchMove(evt: TouchEvent){
-    if (this.startPosition == 0) {
-      this.startPosition = evt.touches[0].clientY;
-    }
-    this.height = document.querySelector(".bottomSheet").clientHeight;
-    var y = evt.touches[0].clientY;
-    this.currentPosition = y - this.startPosition;
-    if (this.currentPosition > 0 && this.startPosition > 0) {
-      (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.transform = "translate3d(0px," + this.currentPosition + "px,0px)";
-    }
-  }
-
-  touchEnd(){
-    this.minimumThreshold = this.height - 130;
-    if (this.currentPosition < this.minimumThreshold) {
-      (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.transform = "translate3d(0px,0px,0px)";
-    } else {
-      this.close();
-    }
-  }
-
 
 }
+
+
+
+  /*--------------TRAITEMENT DES DONNEES RGPD----------*/
+  // open(){
+  //   (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.bottom = "0px";
+  //   (<HTMLStyleElement>document.querySelector(".bg")).style.display = "block";
+  // }
+
+  // close(){
+  //   this.currentPosition = 0;
+  //   this.startPosition = 0;
+  //   (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.bottom = "-1000px";
+  //   (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.transform = "translate3d(0,0,0)";
+  //   (<HTMLStyleElement>document.querySelector(".bg")).style.display = "none";
+
+  // }
+
+  // touchMove(evt: TouchEvent){
+  //   if (this.startPosition == 0) {
+  //     this.startPosition = evt.touches[0].clientY;
+  //   }
+  //   this.height = document.querySelector(".bottomSheet").clientHeight;
+  //   var y = evt.touches[0].clientY;
+  //   this.currentPosition = y - this.startPosition;
+  //   if (this.currentPosition > 0 && this.startPosition > 0) {
+  //     (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.transform = "translate3d(0px," + this.currentPosition + "px,0px)";
+  //   }
+  // }
+
+  // touchEnd(){
+  //   this.minimumThreshold = this.height - 130;
+  //   if (this.currentPosition < this.minimumThreshold) {
+  //     (<HTMLStyleElement>document.querySelector(".bottomSheet")).style.transform = "translate3d(0px,0px,0px)";
+  //   } else {
+  //     this.close();
+  //   }
+  // }
